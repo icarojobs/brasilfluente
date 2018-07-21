01991089e76c8b55a29c98d8f9b9a76810468973
Icaro William <mob2you@gmail.com>
2018-07-20 21:42:43 -0300

    removido menus da home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -81,13 +81,6 @@
                 <div class="title m-b-md">
                     {{ config('app.name') }}
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
